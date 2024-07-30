624909895de4037e761e7a2062078dc284c79887
Matt Magoffin <matt@solarnetwork.net>
2024-07-30 17:54:58 +1200

    Fix typos in comments.

--- api/src/main/java/net/solarnetwork/io/modbus/ModbusMessage.java
+++ api/src/main/java/net/solarnetwork/io/modbus/ModbusMessage.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * ModbusMessage.java - 25/11/2022 3:10:59 pm
- * 
+ *
  * Copyright 2022 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -24,7 +24,7 @@ package net.solarnetwork.io.modbus;
 
 /**
  * API for a Modbus message.
- * 
+ *
  * @author matt
  * @version 1.0
  */
@@ -32,32 +32,32 @@ public interface ModbusMessage {
 
 	/**
 	 * Get the device unit ID.
-	 * 
+	 *
 	 * <p>
 	 * Modbus allows only values from 0-255 for the unit ID.
 	 * </p>
-	 * 
+	 *
 	 * @return the unit ID, as an integer
 	 */
 	int getUnitId();
 
 	/**
 	 * Get the Modbus function code.
-	 * 
+	 *
 	 * @return the function code, never {@literal null}
 	 */
 	ModbusFunction getFunction();
 
 	/**
 	 * Get the Modbus error code.
-	 * 
+	 *
 	 * @return the error code, or {@literal null} if not an error
 	 */
 	ModbusError getError();
 
 	/**
 	 * Test if this message is an exception (error).
-	 * 
+	 *
 	 * @return {@literal true} if {@link #getError()} it not {@literal null}
 	 */
 	default boolean isException() {
@@ -66,16 +66,16 @@ public interface ModbusMessage {
 
 	/**
 	 * Unwrap this message as a specific message type, if possible.
-	 * 
+	 *
 	 * <p>
-	 * This message should be used instead of relying on a direct
+	 * This method should be used instead of relying on a direct
 	 * {@code instanceof} operator, because the actual Modbus message may be
 	 * encapsulated in some way. For example, instead of trying this:
 	 * </p>
-	 * 
+	 *
 	 * <pre>
 	 * <code>
-	 * // WRONG WAY: DO NOT TRY THIS 
+	 * // WRONG WAY: DO NOT TRY THIS
 	 * ModbusMessage msg = getMessageFromSomewhere();
 	 * if ( msg instanceof RegistersModbusMessage ) {
 	 *   RegistersModbusMessage r = (RegistersModbusMessage)msg;
@@ -83,11 +83,11 @@ public interface ModbusMessage {
 	 * }
 	 * </code>
 	 * </pre>
-	 * 
+	 *
 	 * <p>
 	 * try this instead:
 	 * </p>
-	 * 
+	 *
 	 * <pre>
 	 * <code>
 	 * ModbusMessage msg = getMessageFromSomewhere();
@@ -97,19 +97,19 @@ public interface ModbusMessage {
 	 * }
 	 * </code>
 	 * </pre>
-	 * 
+	 *
 	 * @param <T>
-	 *        the message type to unwrap to
+	 *        the message type to unwrap
 	 * @param msgType
 	 *        the class to unwrap as
-	 * @return the reply message as the given type, or {@literal null} if the
-	 *         reply is not compatible with {@code msgType}
+	 * @return the message as the given type, or {@literal null} if this message
+	 *         is not compatible with {@code msgType}
 	 */
 	<T extends ModbusMessage> T unwrap(Class<T> msgType);
 
 	/**
 	 * Compare the "sameness" of this message to another.
-	 * 
+	 *
 	 * @param obj
 	 *        the message to compare to this message
 	 * @return if the class of {@code other} is the same as this class, and the
